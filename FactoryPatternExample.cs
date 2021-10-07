0d9108d8a629af2a2b0c576adfcc2996be162276
Nicholas Larsen <nlarsen@access-healthservices.com>
2021-10-07 13:17:10 -0500

    Added Comments for clarity

--- FactoryPatternExample.cs
+++ FactoryPatternExample.cs
@@ -3,6 +3,17 @@ using FactoryPatternExample.Objects;
 
 namespace FactoryPatternExample
 {
+    /*
+    * The majority of this code is mocked or directly taken from the Derek Banas YouTube series in
+    * an effort to teach myself and get familiar with these patterns.  None of this is meant to be
+    * original content, and if you see this and wonder why it's in my repo, mostly it's for me, but
+    * I'm happy that you're here and here's proof that I have at least heard of this particular
+    * pattern!
+    *
+    * Author: Nicholas Larsen
+    * Date: 2021/10/07
+    */
+    
     // ReSharper disable once ClassNeverInstantiated.Global
     internal class FactoryPatternExample
     {
@@ -11,6 +22,8 @@ namespace FactoryPatternExample
             Console.WriteLine("B/U/R?");
             try
             {
+                // Factory patterns are used to create classes dynamically, in this case
+                // Classes are created based on user input. This line is the crux of the code
                 var enemyShip = EnemyShipFactory.MakeEnemyShip(Console.ReadLine() ?? "");
                 TestShipFunctionality(enemyShip);
             }
--- Objects/EnemyShipFactory.cs
+++ Objects/EnemyShipFactory.cs
@@ -4,6 +4,8 @@ namespace FactoryPatternExample.Objects
 {
     public static class EnemyShipFactory
     {
+        // By encapsulating the logic to one class and method, it allows for cleaner reading
+        // and better functionality when opposed to putting all this in the main code.
         public static EnemyShip MakeEnemyShip(string shipType)
         {
             var sanitizedShipTypeInput = shipType.ToUpper().ToCharArray()[0];
